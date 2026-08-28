ee8981264a5fa594353230140a5b4ce5348464bb
kinnnine <nakaharaml9@gmail.com>
2026-08-29 02:21:57 +0700

    proxy.ts: relocate debug log to capture EAG tags

--- src/proxy.ts
+++ src/proxy.ts
@@ -296,9 +296,9 @@ export class EaglerProxy {
 						let fard = packet.copy();
 						fard.readVarInt();
 						let tag = fard.readString();
-						// debug
-						console.log(tag);
 						if (tag.startsWith("EAG|")) {
+							// debug
+							console.log(tag);
 							if (tag == "EAG|Skins-1.8" || tag == "EAG|Capes-1.8") {
 								let isCape = tag[4] == "C";
 								let data = fard.take(fard.length);
